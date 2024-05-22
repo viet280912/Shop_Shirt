f5c266feca1ee51e048eadf47738a1571bee53df
phucxu <phuctxp152002@gmail.com>
2024-05-22 11:40:01 +0700

    add file Address Repository

---
+++ src/main/java/com/example/firstproject/model/Address/AddressRepository.java
@@ -0,0 +1,6 @@
+package com.example.firstproject.model.Address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+}
