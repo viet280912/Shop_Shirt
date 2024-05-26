395a80c57685487c1105345efa933475a074d0be
phucxu <phuctxp152002@gmail.com>
2024-05-26 15:04:46 +0700

    phucxu - update quantity of cartItem v1.0

--- src/main/java/com/example/firstproject/model/CartItem/CartItemServiceImpl.java
+++ src/main/java/com/example/firstproject/model/CartItem/CartItemServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.firstproject.model.CartItem;
 
 import com.example.firstproject.dto.CartItemDTO;
+import com.example.firstproject.exception.NotFoundException;
 import com.example.firstproject.mapper.CartItemMapper;
 import com.example.firstproject.model.Cart.Cart;
 import com.example.firstproject.model.Cart.CartRepository;
+import com.example.firstproject.model.ProductDetail.ProductDetail;
+import com.example.firstproject.model.ProductDetail.ProductDetailRepository;
 import com.example.firstproject.model.User.User;
 import com.example.firstproject.model.User.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -22,6 +25,8 @@ public class CartItemServiceImpl implements CartItemService{
     private UserRepository userRepository;
     @Autowired
     private CartItemMapper cartItemMapper;
+    @Autowired
+    private ProductDetailRepository productDetailRepository;
     @Override
     public List<CartItem> getAllCartItemByUserId(Integer userId) {
         User user = userRepository.findById(userId)
@@ -38,9 +43,8 @@ public class CartItemServiceImpl implements CartItemService{
     @Override
     public CartItem updateCartItemById(CartItemDTO cartItemDTO) {
         CartItem cartItemUpdating = cartItemRepository.findById(cartItemDTO.getId())
-                .orElseThrow(() -> new EntityNotFoundException("Not found cartitem with id: " + cartItemDTO.getId()));
-        CartItem cartItemMapped = cartItemMapper.convertToCartItem(cartItemDTO);
-        cartItemUpdating.setQuantity(cartItemMapped.getQuantity());
+                .orElseThrow(() -> new EntityNotFoundException("Not found cartItem with id: " + cartItemDTO.getId()));
+        cartItemUpdating.setQuantity(cartItemDTO.getQuantity());
         cartItemRepository.save(cartItemUpdating);
         return cartItemUpdating;
     }
@@ -48,11 +52,16 @@ public class CartItemServiceImpl implements CartItemService{
     @Override
     public CartItem createCartItem(CartItemDTO cartItemDTO) {
         CartItem cartItem = new CartItem();
-        CartItem cartItemMapped = cartItemMapper.convertToCartItem(cartItemDTO);
-        cartItem.setCartItem_id(cartItemDTO.getId());
-        cartItem.setCart(cartItemMapped.getCart());
-        cartItem.setQuantity(cartItemMapped.getQuantity());
-        cartItem.setProductDetail(cartItemMapped.getProductDetail());
+        cartItem.setCartItem_id(0);
+        User user = userRepository.findById(cartItemDTO.getUserId())
+                        .orElseThrow(() -> new NotFoundException("User not found with id: "+ cartItemDTO.getUserId()));
+        Cart cart = cartRepository.findCartByUser(user)
+                .orElseThrow(() -> new NotFoundException("Cart not found with user id: " + cartItemDTO.getUserId()));
+        cartItem.setCart(cart);
+        cartItem.setQuantity(cartItemDTO.getQuantity());
+        ProductDetail productDetail = productDetailRepository.findById(cartItemDTO.getProductDetailId())
+                        .orElseThrow(() -> new NotFoundException("Product detail not found with id: " + cartItemDTO.getProductDetailId()));
+        cartItem.setProductDetail(productDetail);
         cartItemRepository.save(cartItem);
         return cartItem;
     }
--- src/test/java/com/example/firstproject/cartItem/CartItemServiceTest.java
+++ src/test/java/com/example/firstproject/cartItem/CartItemServiceTest.java
@@ -5,19 +5,25 @@ import com.example.firstproject.model.Cart.Cart;
 import com.example.firstproject.model.CartItem.CartItem;
 import com.example.firstproject.model.CartItem.CartItemRepository;
 import com.example.firstproject.model.CartItem.CartItemService;
+import com.example.firstproject.model.Product.Product;
+import com.example.firstproject.model.Product.ProductRepository;
 import com.example.firstproject.model.ProductDetail.ProductDetail;
+import com.example.firstproject.model.ProductDetail.ProductDetailRepository;
 import com.example.firstproject.model.User.User;
 import com.example.firstproject.model.User.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -30,26 +36,92 @@ public class CartItemServiceTest {
     private CartItemService cartItemService;
     @Autowired
     private UserRepository userRepository;
-    @Test
-    void createCartItemTest(){
-        //given
-        User user = new User();
-        user.setUser_id(0);
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private ProductDetailRepository productDetailRepository;
+    private User user;
+    private Cart cart;
+    private Product product1, product2, product3;
+    private ProductDetail productDetail1, productDetail2, productDetail3, productDetail4, productDetail5, productDetail6;
+
+    @BeforeEach
+    void setUp(){
+        user = new User();
         user.setFullName("full name");
         user.setUserName("anhdaychap12");
         user.setPassword("12345678");
         user.setEmail("phuctxp15200@gmail.com");
-        Cart cart = new Cart();
+        cart = new Cart();
         cart.setCreated_At(Date.valueOf(LocalDate.now()));
         cart.setCartItems(new ArrayList<>());
         cart.setUser(user);
         user.setCart(cart);
-        cart.setUser(user);
         userRepository.save(user);
-        CartItemDTO cartItemDTO = new CartItemDTO();
-        cartItemDTO.setId(0);
-        cartItemDTO.setUserId(cart.getUser().getUser_id());
-        cartItemDTO.setQuantity(5);
+
+        //given
+        product1 = new Product();
+        product1.setProduct_name("product1");
+        productRepository.save(product1);
+        product2 = new Product();
+        product2.setProduct_name("product2");
+        productRepository.save(product2);
+        product3 = new Product();
+        product3.setProduct_name("product3");
+        productRepository.save(product3);
+
+        productDetail1 = new ProductDetail();
+        productDetail1.setProduct(product1);
+        productDetail1.setColor("#Color1");
+        productDetail1.setSize("M");
+        productDetail1.setStock(20);
+        productDetailRepository.save(productDetail1);
+
+        productDetail2 = new ProductDetail();
+        productDetail2.setProduct(product1);
+        productDetail2.setColor("#Color1");
+        productDetail2.setSize("L");
+        productDetail2.setStock(20);
+        productDetailRepository.save(productDetail2);
+
+        productDetail3 = new ProductDetail();
+        productDetail3.setProduct(product1);
+        productDetail3.setColor("#Color1");
+        productDetail3.setSize("XL");
+        productDetail3.setStock(20);
+        productDetailRepository.save(productDetail3);
+
+        productDetail4 = new ProductDetail();
+        productDetail4.setProduct(product2);
+        productDetail4.setColor("#Color1");
+        productDetail4.setSize("XL");
+        productDetail4.setStock(20);
+        productDetailRepository.save(productDetail4);
+
+        productDetail5 = new ProductDetail();
+        productDetail5.setProduct(product2);
+        productDetail5.setColor("#Color1");
+        productDetail5.setSize("XL");
+        productDetail5.setStock(20);
+        productDetailRepository.save(productDetail5);
+
+        productDetail6 = new ProductDetail();
+        productDetail6.setProduct(product2);
+        productDetail6.setColor("#Color1");
+        productDetail6.setSize("XL");
+        productDetail6.setStock(20);
+        productDetailRepository.save(productDetail6);
+
+
+
+    }
+    @Test
+    @Rollback
+    void createCartItemTest(){
+       CartItemDTO cartItemDTO = new CartItemDTO();
+       cartItemDTO.setProductDetailId(productDetail1.getProductDetail_id());
+       cartItemDTO.setQuantity(5);
+       cartItemDTO.setUserId(user.getUser_id());
 
         //when
         CartItem result = cartItemService.createCartItem(cartItemDTO);
@@ -59,5 +131,51 @@ public class CartItemServiceTest {
         //then
         assertEquals(ex.getCart().getCart_id(), result.getCart().getCart_id());
         assertEquals(ex.getQuantity(), result.getQuantity());
+    }
+    @Test
+    @Rollback
+    void getAllCartItemByUserId_Success(){
+        CartItemDTO cartItemDTO1 = new CartItemDTO();
+        cartItemDTO1.setUserId(user.getUser_id());
+        cartItemDTO1.setQuantity(1);
+        cartItemDTO1.setProductDetailId(productDetail1.getProductDetail_id());
+        CartItem cartItem1 = cartItemService.createCartItem(cartItemDTO1);
+
+        CartItemDTO cartItemDTO2 = new CartItemDTO();
+        cartItemDTO2.setUserId(user.getUser_id());
+        cartItemDTO2.setQuantity(3);
+        cartItemDTO2.setProductDetailId(productDetail2.getProductDetail_id());
+        CartItem cartItem2 = cartItemService.createCartItem(cartItemDTO2);
+
+        CartItemDTO cartItemDTO3 = new CartItemDTO();
+        cartItemDTO3.setUserId(user.getUser_id());
+        cartItemDTO3.setQuantity(3);
+        cartItemDTO3.setProductDetailId(productDetail3.getProductDetail_id());
+        CartItem cartItem3 = cartItemService.createCartItem(cartItemDTO3);
+
+        //when
+        List<CartItem> rs = cartItemService.getAllCartItemByUserId(user.getUser_id());
+
+        //then
+        assertEquals(3, rs.size());
+    }
+    @Test
+    @Rollback
+    void updateCartItemById_Success(){
+        //given
+        CartItemDTO cartItemDTO1 = new CartItemDTO();
+        cartItemDTO1.setUserId(user.getUser_id());
+        cartItemDTO1.setQuantity(1);
+        cartItemDTO1.setProductDetailId(productDetail1.getProductDetail_id());
+        CartItem cartItem1 = cartItemService.createCartItem(cartItemDTO1);
+
+        //when
+        cartItemDTO1.setId(cartItem1.getCartItem_id());
+        cartItemDTO1.setQuantity(10);
+        CartItem rs = cartItemService.updateCartItemById(cartItemDTO1);
+        //then
+
+        assertEquals(10, rs.getQuantity());
+
     }
 }
