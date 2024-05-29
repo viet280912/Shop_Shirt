64d3c2bb041cf655af8d1d2180a7ab577cbe09f1
phucxu <phuctxp152002@gmail.com>
2024-05-29 15:25:01 +0700

    phucxu - update cartItem

--- src/main/java/com/example/firstproject/controller/CartItemController.java
+++ src/main/java/com/example/firstproject/controller/CartItemController.java
@@ -1,24 +1,36 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.CartItemDTO;
 import com.example.firstproject.model.CartItem.CartItem;
 import com.example.firstproject.model.CartItem.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CartItemController {
     @Autowired
     private CartItemService cartItemService;
-//    @GetMapping("user/{userId}/cart/items")
-//    public ResponseEntity<?> getAllItemInCartByUserId(
-//            @PathVariable Integer userId
-//    ){
-////        Page<CartItem> cartItems = cartItemService.getAllCartItemByUserId(userId);
-//    }
+    @GetMapping("user/{userId}/cart/items")
+    public ResponseEntity<?> getAllItemInCartByUserId(
+            @PathVariable Integer userId,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
+    ){
+        Page<CartItem> cartItems = cartItemService.getAllCartItemByUserId(userId, page, size);
+        return new ResponseEntity<>(cartItems, HttpStatus.OK);
+    }
+
+    @PutMapping("cart/items/{id}")
+    public ResponseEntity<?> updateItemInCart(
+            @PathVariable Integer id,
+            @RequestBody CartItemDTO cartItemDTO
+            ){
+        return new ResponseEntity<>(cartItemService.updateCartItemById(id, cartItemDTO), HttpStatus.OK);
+    }
 }
--- src/test/java/com/example/firstproject/cartItem/CartItemControllerTest.java
+++ src/test/java/com/example/firstproject/cartItem/CartItemControllerTest.java
@@ -11,43 +11,53 @@ import com.example.firstproject.model.ProductDetail.ProductDetail;
 import com.example.firstproject.model.ProductDetail.ProductDetailRepository;
 import com.example.firstproject.model.User.User;
 import com.example.firstproject.model.User.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
-public class CartItemServiceTest {
+public class CartItemControllerTest {
     @Autowired
     private CartItemRepository cartItemRepository;
     @Autowired
-    private CartItemService cartItemService;
-    @Autowired
-    private UserRepository userRepository;
+    private ProductDetailRepository productDetailRepository;
     @Autowired
     private ProductRepository productRepository;
     @Autowired
-    private ProductDetailRepository productDetailRepository;
+    private UserRepository userRepository;
+    @Autowired
+    private CartItemService cartItemService;
+    @Autowired
+    private MockMvc mockMvc;
+
     private User user;
     private Cart cart;
     private Product product1, product2, product3;
     private ProductDetail productDetail1, productDetail2, productDetail3, productDetail4, productDetail5, productDetail6;
-
     @BeforeEach
-    void setUp(){
+    public void setUp(){
         user = new User();
         user.setFullName("full name");
         user.setUserName("anhdaychap12");
@@ -112,30 +122,10 @@ public class CartItemServiceTest {
         productDetail6.setSize("XL");
         productDetail6.setStock(20);
         productDetailRepository.save(productDetail6);
-
-
-
     }
     @Test
     @Rollback
-    void createCartItemTest(){
-       CartItemDTO cartItemDTO = new CartItemDTO();
-       cartItemDTO.setProductDetailId(productDetail1.getProductDetail_id());
-       cartItemDTO.setQuantity(5);
-       cartItemDTO.setUserId(user.getUser_id());
-
-        //when
-        CartItem result = cartItemService.createCartItem(cartItemDTO);
-        CartItem ex = cartItemRepository.findById(result.getCartItem_id())
-                .orElseThrow(() -> new EntityNotFoundException("Not found cartItem"));
-
-        //then
-        assertEquals(ex.getCart().getCart_id(), result.getCart().getCart_id());
-        assertEquals(ex.getQuantity(), result.getQuantity());
-    }
-    @Test
-    @Rollback
-    void getAllCartItemByUserId_Success(){
+    void getAllCartItemByUserId() throws Exception {
         CartItemDTO cartItemDTO1 = new CartItemDTO();
         cartItemDTO1.setUserId(user.getUser_id());
         cartItemDTO1.setQuantity(1);
@@ -154,29 +144,41 @@ public class CartItemServiceTest {
         cartItemDTO3.setProductDetailId(productDetail3.getProductDetail_id());
         CartItem cartItem3 = cartItemService.createCartItem(cartItemDTO3);
 
-        //when
-        Page<CartItem> rs = cartItemService.getAllCartItemByUserId(user.getUser_id(), 1, 2);
-
         //then
-        assertEquals(3, rs.getTotalElements());
+        mockMvc.perform(get("/api/user/"+user.getUser_id()+"/cart/items")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalElements", is(3)));
     }
     @Test
     @Rollback
-    void updateCartItemById_Success(){
-        //given
+    void updateCartItemById_Success() throws Exception{
         CartItemDTO cartItemDTO1 = new CartItemDTO();
         cartItemDTO1.setUserId(user.getUser_id());
         cartItemDTO1.setQuantity(1);
         cartItemDTO1.setProductDetailId(productDetail1.getProductDetail_id());
         CartItem cartItem1 = cartItemService.createCartItem(cartItemDTO1);
 
-        //when
-        cartItemDTO1.setId(cartItem1.getCartItem_id());
-        cartItemDTO1.setQuantity(10);
-        CartItem rs = cartItemService.updateCartItemById(cartItem1.getCartItem_id(),cartItemDTO1);
-        //then
+        CartItemDTO cartItemDTO2 = new CartItemDTO();
+        cartItemDTO2.setUserId(user.getUser_id());
+        cartItemDTO2.setQuantity(3);
+        cartItemDTO2.setProductDetailId(productDetail2.getProductDetail_id());
+        CartItem cartItem2 = cartItemService.createCartItem(cartItemDTO2);
 
-        assertEquals(10, rs.getQuantity());
+        CartItemDTO cartItemDTO3 = new CartItemDTO();
+        cartItemDTO3.setUserId(user.getUser_id());
+        cartItemDTO3.setQuantity(3);
+        cartItemDTO3.setProductDetailId(productDetail3.getProductDetail_id());
+        CartItem cartItem3 = cartItemService.createCartItem(cartItemDTO3);
+
+        cartItemDTO1.setQuantity(3);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        mockMvc.perform(put("/api/cart/items/"+cartItem1.getCartItem_id())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(cartItem3)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.quantity", is(3)));
 
     }
 }
